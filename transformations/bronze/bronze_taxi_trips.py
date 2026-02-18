c2b761b33ca40a95e78db3aca6bc27dfb0c28988
Xhadee <khadiabsatou@gmail.com>
2026-02-18 20:08:16 +0000

    bronze corrected

--- transformations/bronze/bronze_taxi_trips.py
+++ transformations/bronze/bronze_taxi_trips.py
@@ -1,4 +1,5 @@
 import dlt as dp
+from pyspark.sql import functions as F
 
 @dp.table(
     name="bronze_taxi_trips",
@@ -6,17 +7,21 @@ import dlt as dp
 )
 def bronze_taxi_trips():
     """
-    Bronze layer: Raw taxi trip data ingestion
-    Correction : Ajout de la gestion du format timestampNtz pour Delta Lake.
+    Bronze layer : Ingestion avec correction du type timestampNtz.
     """
     source_path = "/Volumes/taxi_mlops_prod/taxi_analytics/yellowdata"
     
+    # On définit explicitement les options pour éviter le conflit de schéma
     return (
         spark.readStream.format("cloudFiles")
         .option("cloudFiles.format", "parquet")
-        # Ces deux options règlent le problème du timestampNtz
+        # On force Spark à utiliser le format de timestamp standard (LTZ)
         .option("spark.sql.parquet.v2.datetimeRebaseModeInRead", "CORRECTED")
-        .option("spark.sql.parquet.int96RebaseModeInRead", "CORRECTED")
         .option("cloudFiles.inferColumnTypes", "true")
+        # Cette option force le schéma à rester compatible avec les tables Delta standards
+        .option("cloudFiles.schemaEvolutionMode", "rescue")
         .load(source_path)
+        # Sécurité supplémentaire : on convertit explicitement les colonnes de temps
+        .withColumn("tpep_pickup_datetime", F.col("tpep_pickup_datetime").cast("timestamp"))
+        .withColumn("tpep_dropoff_datetime", F.col("tpep_dropoff_datetime").cast("timestamp"))
     )
